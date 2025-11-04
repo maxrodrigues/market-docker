52e6d0a26e42e4c975fc7a7a959572c50b55db0a
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-11-04 08:27:25 -0300

    raw: finished repository clone script

--- utils/repos.sh
+++ utils/repos.sh
@@ -5,14 +5,25 @@ source ./utils/config.sh
 draw_line_separator
 echo -e "===== CLONANDO REPOSITORIOS ====="
 
-repos_list=$(tr -d '\n' < ./utils/json/repos.json | tr -d '[]" ' | sed 's/,/ /g')
-read -a REPOS_TO_CREATE <<< "$repos_list"
+# read json repos file
+repos_list=$(tr -d '\n' < ./utils/json/repos.json | sed -E 's/[][]//g; s/"//g; s/,/ /g')
 
-for repos in "${REPOS_TO_CREATE[@]}"; do
-  if [ ! -e "./${repos}" ]; then
-    echo -e $repos
+# transform in array
+read -r -a REPOS_TO_CREATE <<< "$repos_list"
+
+# count repos
+echo "Repositórios encontrados: ${#REPOS_TO_CREATE[@]}"
+
+# interact with found repos
+for i in "${!REPOS_TO_CREATE[@]}"; do
+  # check repo exists
+  if [ ! -e "./${REPOS_TO_CREATE[$i]}" ]; then
+    # message and clone repo
+    echo -e "Clonando repositório: ${REPOS_TO_CREATE[$i]}"
+    git clone git@github.com:maxrodrigues/${REPOS_TO_CREATE[$i]}
   else
+    # wait and message
     sleep 1
-    printf "\n ${information}O projeto ${repos} já existe.${nocolor}"
+    echo -e "O projeto ${REPOS_TO_CREATE[$i]} já existe."
   fi
 done
